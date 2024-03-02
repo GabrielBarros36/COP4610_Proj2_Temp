c2fb3ef21902b836b77a2777c65cf5c289fee6da
Gabriel Romanini de Barros~ <gabriel.romanini.barros@gmail.com~>
2024-03-01 21:25:31 -0500

    Corrections

--- part2/src/my_timer.c
+++ part2/src/my_timer.c
@@ -110,7 +110,7 @@ static ssize_t timer_read(struct file *file, char __user *ubuf, size_t count, lo
 		len = snprintf(buf, sizeof(buf), "current time: %lld.%lld\n", (long long)ts_now.tv_sec, (long long)ts_now.tv_nsec);
 	else
 		
-		len = snprintf(buf, sizeof(buf), "current time: %lld.%lld\nelapsed time: %lld.%lld\n", (long long)ts_now.tv_sec, (long long)i(ts_now.tv_nsec) , secDiff, nanoDiff);
+		len = snprintf(buf, sizeof(buf), "current time: %lld.%lld\nelapsed time: %lld.%lld\n", (long long)ts_now.tv_sec, (long long)(ts_now.tv_nsec) , secDiff, nanoDiff);
 
 	latestSec = ts_now.tv_sec;
 	latestNs = ts_now.tv_nsec;
