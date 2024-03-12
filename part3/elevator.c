c6a5a264d96e793e5bd4fcfd63ed4c324435d252
Gabriel Romanini de Barros <gabriel.romanini.barros@gmail.com>
2024-03-12 16:28:54 -0400

    Made issue_request WITHOUT kthreads or mutex

--- part3/elevator.c
+++ part3/elevator.c
@@ -22,9 +22,87 @@ MODULE_VERSION("1.0");
 #define ENTRY_PERMS 0666
 #define PARENT NULL
 #define ELEVATOR_LIMIT 5        //max passengers in elevator
+#define WEIGHT_LIMIT 7
 #define ELEVATOR_MAX_FLOOR 5    //max floor in building
 
 static struct proc_dir_entry* elevator_entry;
+
+//Keeps track of the passengers INSIDE the elevator
+struct {
+    int total_cnt;
+    int total_length;
+    int total_weight_int;
+    int total_weight_dec;
+    struct list_head list;
+} passengers;
+
+//Keeps track of the passengers WAITING for the elevator
+struct{
+    int total_cnt;
+    struct list_head list;
+} passenger_queue;
+
+typedef struct passenger{
+    int id;
+    int weight_int;
+    int weight_dec;
+    struct list_head list;
+} Passenger;
+
+//Our implementation of the start_elevator syscall
+extern int (*STUB_start_elevator)(void);
+int custom_start_elevator(){
+
+}
+
+//Our implementation of the issue_request syscall
+extern int (*STUB_issue_request)(int, int, int);
+int custom_issue_request(int passengerType, int startFloor, int destinationFloor){
+
+    Passenger *a;
+    a = kmalloc(sizeof(Passenger) * 1, __GFP_RECLAIM);
+    a->id = passengerType;
+
+    switch(passengerType){
+        //Visitor, 0.5 lbs
+        case 0:
+            a->weight_int = 0;
+            a->weight_dec = 5;
+            break;
+
+        //Part-time, 1 lb    
+        case 1:
+            a->weight_int = 1;
+            a->weight_dec = 0;
+            break;
+
+        //Lawyers, 1.5 lbs
+        case 2:
+            a->weight_int = 1;
+            a->weight_dec = 5;
+            break;
+        
+        //Boss, 2 lbs
+        case 3:
+            a->weight_int = 2;
+            a->weight_dec = 0;
+            break;
+        default:
+            return -1;
+    }
+
+    //Add passenger to end of queue of waiting passengers
+    list_add_tail(&a->list, &passenger_queue.list);
+    passenger_queue.total_cnt++;
+
+    return 0;
+}
+
+//Our implementation of the stop_elevator syscall
+extern int (*STUB_stop_elevator)(void);
+int custom_stop_elevator(){
+    
+}
 
 static ssize_t elevator_read(struct file *file, char __user *ubuf, size_t count, loff_t *ppos)
 {
@@ -42,12 +120,14 @@ static ssize_t elevator_read(struct file *file, char __user *ubuf, size_t count,
     return simple_read_from_buffer(ubuf, count, ppos, buf, len);
 }
 
+
 static const struct proc_ops elevator_fops = {
     .proc_read = elevator_read,
 };
 
 static int __init elevator_init(void){
-    //proc file operations
+
+    //proc file operations below
     elevator_entry = proc_create(ENTRY_NAME, ENTRY_PERMS, PARENT, &elevator_fops);
     if (elevator_entry == NULL){
         return -ENOMEM;
@@ -55,16 +135,26 @@ static int __init elevator_init(void){
 
     //insert rest of init operations here
 
+    //Initializes empty passenger queue
+    passenger_queue.total_cnt = 0;
+    INIT_LIST_HEAD(&passenger_queue.list);
+
+    //Initializes list for empty elevator
+    passengers.total_cnt = 0;
+    passengers.total_length = 0;
+    passengers.total_weight_int = 0;
+    passengers.total_weight_dec = 0;
+    INIT_LIST_HEAD(&passengers.list);
+
     return 0;
 }
 
-static viud __exit elevator_exit(void){
-    //proc file operation
+static void __exit elevator_exit(void){
+    //proc file operation below
     proc_remove(elevator_entry);
 
     //insert rest of exit operations here
 }
-
 
 module_init(elevator_init);
 module_exit(elevator_exit);
